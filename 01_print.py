6de4de4e47a11f6990c6f64d92f0e3e493ac8067
Santiago Salas <santiago.salas.contractor@bbva.com>
2022-12-28 18:52:44 -0500

    Variables

---
+++ 01_print.py
@@ -0,0 +1,11 @@
+# Print console
+print("Hello world this is Python Language!")
+print("My name is Santiago")
+
+""" Calculator Basic Operations, 
+    Python"""
+print(15 + 5)
+print(10 - 2)
+print(2 * 2)
+print(10 / 3)
+
---
+++ 02_vars.py
@@ -0,0 +1,24 @@
+
+# Vars in Python
+
+print("Hola, VARS")
+
+my_name = "Santiago"
+my_lastName = "Salas"
+my_age = 25
+
+print(my_name + " " + my_lastName, my_age)
+print(my_age)
+
+# Las variables pueden ser re-asignadas en otro momento aunque existan antes
+my_name = "Nicolas"
+print("Aqui cambia el valor", my_name)
+print(my_name)
+
+# input, para capturar datos desde la terminal
+
+my_name = input("Cual es el nombre?")
+print("El nombre digitado es: ", my_name)
+
+my_age = input("Cual es su edad?")
+print("La edad digitada es: ", my_age)
